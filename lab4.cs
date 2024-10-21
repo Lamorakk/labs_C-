c142a95090598a6099891f22baeb450d5faffbae
Nazar <129693758+Lamorakk@users.noreply.github.com>
2024-10-21 14:20:00 +0300

    Add files via upload

---
+++ lab4.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+public interface IShape
+{
+    double CalculateArea();
+}
+
+public class Shape : IShape
+{
+    public string Name { get; set; }
+
+    public Shape(string name)
+    {
+        Name = name;
+    }
+
+    public virtual void Display()
+    {
+        Console.WriteLine($"Shape: {Name}");
+    }
+
+    public virtual double CalculateArea()
+    {
+        return 0.0;
+    }
+}
+
+public class Circle : Shape
+{
+    public double Radius { get; set; }
+
+    public Circle(double radius) : base("Circle")
+    {
+        Radius = radius;
+    }
+
+    public override double CalculateArea()
+    {
+        return Math.PI * Math.Pow(Radius, 2); // Площа = π * r^2
+    }
+
+    public override void Display()
+    {
+        base.Display();
+        Console.WriteLine($"Radius: {Radius}");
+        Console.WriteLine($"Area: {CalculateArea()}");
+    }
+}
+
+public class lab4
+{
+    public static void Main(string[] args)
+    {
+        Circle circle = new Circle(5.0);
+        circle.Display(); 
+    }
+}
